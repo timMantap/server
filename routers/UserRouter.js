4b5df1ac6ca96f0acd199c662f9e09c7a14eba5a
maxhamz <maxwell.hamzah@gmail.com>
2020-04-03 14:30:53 +0700

    authentication canceleld

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,11 +1,11 @@
 const UserRouter = require('express').Router()
 const UserController = require('../controllers/UserController.js')
-const {authentication} = require('../middleware/authentication.js')
+// const {authentication} = require('../middleware/authentication.js')
 
-
+UserRouter.get('/', UserController.read)
 UserRouter.post('/register', UserController.register)
 UserRouter.post('/login', UserController.login)
 UserRouter.post('/googleLogin', UserController.googleLogin)
-UserRouter.get('/', authentication, UserController.read)
+// UserRouter.get('/', authentication, UserController.read)
 
 module.exports = UserRouter
